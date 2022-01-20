ed82036a16592f7a111a974f6e32972ac2ac9a88
Alfredo S. Longarini <alfredo.s.longarini@gmail.com>
2022-01-20 02:06:12 -0300

    agregando navBar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar/navBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
+        <NavBar />
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          E-Commerce App
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
---
+++ src/components/NavBar/navBar.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Navbar, NavbarBrand, NavbarToggler, Collapse, Nav, NavItem, NavLink, NavbarText } from 'reactstrap';
+
+import './navBar.css';
+import cartLogo from './shopping-cart.png';
+import logo from './logoPag.png';
+
+const navBar = () => {
+    return (
+        <div>
+            <Navbar
+                color="light"
+                expand="md"
+                light
+            >
+                <NavbarBrand href="/">
+                    <img className='logoImage' src={logo} alt='logo'></img>
+                </NavbarBrand>
+                <NavbarToggler onClick={function noRefCheck() { }} />
+                <Collapse navbar>
+                    <Nav
+                        className="me-auto"
+                        navbar
+                    >
+                        <NavItem className='itemsNav'>
+                            <NavLink href="#">
+                                <h2>Productos</h2>
+                            </NavLink>
+                        </NavItem>
+                        <NavItem className='itemsNav'>
+                            <NavLink href="#">
+                                <h2>Favoritos</h2>
+                            </NavLink>
+                        </NavItem>
+                        <NavItem className='itemsNav'>
+                            <NavLink href="#">
+                                <h2>Compras</h2>
+                            </NavLink>
+                        </NavItem>
+                    </Nav>
+                    <NavbarText>
+                        <button type='button'><img className='cartImage' src={cartLogo} alt='cart'></img></button>
+                    </NavbarText>
+                </Collapse>
+            </Navbar>
+        </div>
+    );
+};
+
+export default navBar;
